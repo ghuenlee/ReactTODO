a98d4ecd22ae33eda057e4dd28e3250b8fa68c57
Kyle Sentient <kylesentient@gmail.com>
2016-12-23 19:38:30 +0100

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// Load custom css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+    <h1>Boilerplate!</h1>,
+    document.getElementById('app')
+);
